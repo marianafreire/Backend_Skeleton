4092aa846e9d7cb1f7b63dc83943dbd4a85514cf
marianafreire <mcf2@cin.ufpe.br>
2023-03-24 14:45:26 -0300

    fix: changing get method to test deploy
    Signed-off-by: marianafreire <mcf2@cin.ufpe.br>

--- index.py
+++ index.py
@@ -17,10 +17,14 @@ def process_data():
 @app.route('https://garmin-health-test.vercel.app/', methods=['GET'])
 def show_post_request():
     try:
+        return f'Health Status:', 200
+        '''
         global last_data
         formatted_data = json.dumps(json.loads(last_data), indent=4)
         return f'Health Status:\n{formatted_data}', 200
+        '''
     except Exception as e:
         return f'An error occurred: {str(e)}', 500
+        
 if __name__ == '__main__':
     app.run(debug=True)

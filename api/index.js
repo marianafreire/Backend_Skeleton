d6b76038e765b4cb7f78a278a3cbc647db3ab714
marianafreire <mcf2@cin.ufpe.br>
2023-03-24 16:54:56 -0300

    fix: adding route to index
    Signed-off-by: marianafreire <mcf2@cin.ufpe.br>

--- api/index.js
+++ api/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Router } = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const route = Router()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let lastData = {};
 
-app.post('/post', (req, res) => {
+route.post('/post', (req, res) => {
   try {
     lastData = req.body;
     console.log(lastData);
@@ -19,13 +21,15 @@ app.post('/post', (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
+route.get('/', (req, res) => {
   try {
       res.status(200).send(`Health Status:\n${JSON.stringify(lastData, null, 4)}`);
   } catch (e) {
     res.status(500).send(`An error occurred: ${e.message}`);
   }
 });
+
+app.use(route)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);

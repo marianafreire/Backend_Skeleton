158fc9ae0c3ee20b0f5a814e8c5851a76db3f4ae
marianafreire <mcf2@cin.ufpe.br>
2023-03-28 15:11:49 -0300

    fix: connecting on db
    Signed-off-by: marianafreire <mcf2@cin.ufpe.br>

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Router } = require('express');
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const route = Router()
+const route = Router();
+const url = process.env.MONGODB_URI;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +19,29 @@ route.post('/post', (req, res) => {
   try {
     lastData = req.body;
     console.log(lastData);
-    res.status(200).send('Request received successfully!');
+    const client = new MongoClient(url);
+    client.connect(function(err) {
+      if (err) {
+        console.error(err);
+        res.status(500).send(`An error occurred: ${err.message}`);
+        return;
+      }
+
+      console.log("Connected to MongoDB");
+      const db = client.db();
+      const myobj = lastData;
+      db.collection("mycollection").insertOne(myobj, function(err, result) {
+        if (err) {
+          console.error(err);
+          res.status(500).send(`An error occurred: ${err.message}`);
+          return;
+        }
+
+        console.log("Data inserted into MongoDB");
+        client.close();
+        res.status(200).send('Request received successfully!');
+      });
+    });
   } catch (e) {
     res.status(500).send(`An error occurred: ${e.message}`);
   }
@@ -29,7 +55,7 @@ route.get('/', (req, res) => {
   }
 });
 
-app.use(route)
+app.use(route);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);

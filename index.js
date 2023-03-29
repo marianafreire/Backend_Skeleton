ad6401b040acf32016b6fdd2468b487e5319852a
marianafreire <mcf2@cin.ufpe.br>
2023-03-29 15:02:13 -0300

    fix: changing schema
    Signed-off-by: marianafreire <mcf2@cin.ufpe.br>

--- index.js
+++ index.js
@@ -24,55 +24,19 @@ mongoose.connect(url, {
   console.error(error);
 });
 
-const dataSchema = new mongoose.Schema(
-  {
-    dailies: [
-      {
-        userId: {type: String, required: true},
-        userAccessToken: {type: String, required: true},
-        summaryId: {type: String, required: true},
-        calendarDate: {type: Date, required: true},
-        activityType: {type: String, required: true},
-        activeKilocalories: {type: Number, default: 0},
-        bmrKilocalories: {type: Number, required: true},
-        steps: {type: Number, default: 0},
-        distanceInMeters: {type: Number, default: 0},
-        durationInSeconds: {type: Number, required: true},
-        activeTimeInSeconds: {type: Number, default: 0},
-        startTimeInSeconds: {type: Number, required: true},
-        startTimeOffsetInSeconds: {type: Number, required: true},
-        moderateIntensityDurationInSeconds: {type: Number, default: 0},
-        vigorousIntensityDurationInSeconds: {type: Number, default: 0},
-        stepsGoal: {type: Number, required: true},
-        intensityDurationGoalInSeconds: {type: Number, required: true},
-        floorsClimbedGoal: {type: Number, required: true},
-        minHeartRateInBeatsPerMinute: {type: Number},
-        maxHeartRateInBeatsPerMinute: {type: Number},
-        averageHeartRateInBeatsPerMinute: {type: Number},
-        restingHeartRateInBeatsPerMinute: {type: Number},
-        timeOffsetHeartRateSamples: {type: Object},
-        averageStressLevel: {type: Number},
-        maxStressLevel: {type: Number},
-        stressDurationInSeconds: {type: Number},
-        restStressDurationInSeconds: {type: Number},
-        activityStressDurationInSeconds: {type: Number},
-        lowStressDurationInSeconds: {type: Number},
-        mediumStressDurationInSeconds: {type: Number},
-        stressQualifier: {type: String},
-      },
-    ],
-  }
+const dailiesSchema = new mongoose.Schema(
+  {dailies: [{}]}
 );
 
-const Data = mongoose.model('Data', dataSchema);
+const Dailies = mongoose.model('Dailies', dailiesSchema);
 
 route.post('/post', async (req, res) => {
   try {
     lastData = req.body;
     console.log(lastData);
-    const myData = new Data(lastData);
-    await myData.save();
-    console.log("Data inserted into MongoDB");
+    const dailies = new Dailies(lastData);
+    await dailies.save();
+    console.log("Dailies inserted into MongoDB");
     return res.status(200).send('Request received successfully!');
   } catch (e) {
     return res.status(500).send(`An error occurred: ${e.message}`);

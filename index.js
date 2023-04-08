b4491233f5eab833ad319b34253556fe096e8c26
marianafreire <mcf2@cin.ufpe.br>
2023-04-08 20:19:41 -0300

    fix: insert new schemas
    Signed-off-by: marianafreire <mcf2@cin.ufpe.br>

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let lastData = {};
 
+// Connect to mongodb
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -24,21 +25,51 @@ mongoose.connect(url, {
   console.error(error);
 });
 
+// create dailies schema
 const dailiesSchema = new mongoose.Schema(
   {dailies: [{}]}
 );
-
 const Dailies = mongoose.model('Dailies', dailiesSchema);
 
+// Trying to create new schemas to indentify different kind of data
+
+// Pulse Ox Summaries
+const pulseOxSchema = new mongoose.Schema(
+  {pulseOx: [{}]}
+);
+const PulseOx = mongoose.model('PulseOx', pulseOxSchema);
+
+// Respiration Summaries
+const respirationSchema = new mongoose.Schema(
+  {allDayRespiration: [{}]}
+);
+const Respiration = mongoose.model('Respiration', respirationSchema);
 
 route.post('/post', async (req, res) => {
   try {
     lastData = req.body;
     console.log(lastData);
-    const dailies = new Dailies(lastData);
-    await dailies.save();
-    console.log("Dailies inserted into MongoDB");
-    return res.status(200).send('Request received successfully!');
+    // check if lastData is dailies, respiration or pulseOx type of data
+
+    if (lastData.hasOwnProperty('dailies')) {
+      const dailies = new Dailies(lastData);
+      await dailies.save();
+      console.log("Dailies inserted into MongoDB");
+      return res.status(200).send('Request received successfully!');
+    } else if (lastData.hasOwnProperty('allDayRespiration')) {
+      const respiration = new Respiration(lastData);
+      await respiration.save();
+      console.log("Respiration inserted into MongoDB");
+      return res.status(200).send('Request received successfully!');
+    } else if (lastData.hasOwnProperty('pulseOx')) {
+      const pulseOx = new PulseOx(lastData);
+      await pulseOx.save();
+      console.log("PulseOx inserted into MongoDB");
+      return res.status(200).send('Request received successfully!');
+    } else {
+      console.log("Unknown data type");
+      return res.status(200).send('Request received successfully!');
+    }
   } catch (e) {
     return res.status(500).send(`An error occurred: ${e.message}`);
   }
